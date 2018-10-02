a2c4f1560c0209249ef11f8d936d2bb4993545fc
Julio Alfonso Rodriguez Olea <julio.rodriguez@cetys.edu.mx>
2018-10-02 16:36:35 -0700

    List Views

--- Basics/App.js
+++ Basics/App.js
@@ -1,82 +1,38 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, ScrollView, Image, TextInput, Button, Alert } from 'react-native';
-
-class Greeting extends Component {
-  render() {
-    return (
-      <Text style = {styles.smallgreen}> Buenos dias {this.props.saludo}!</Text>
-    );
-  }
-}
-
-class Blink extends Component {
-    constructor(props) {
-    super(props);
-    this.state = {isShowingText: true};
-
-    // Toggle the state every second
-    setInterval(() => {
-      this.setState(previousState => {
-        return { isShowingText: !previousState.isShowingText };
-      });
-    }, 1000);
-  }
-
-  render() {
-    let display = this.state.isShowingText ? this.props.text : ' ';
-    return (
-      <Text>{display}</Text>
-    );
-  }
-}
+import { StyleSheet, Text, FlatList, View } from 'react-native';
 
 
 export default class App extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {text: ''};
-  }
- 
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
     return (
-      <ScrollView style={{flex: 1, flexDirection: 'column'}}>
-        <Greeting saludo='Roxanne' />
-        <Greeting saludo='Henry' />
-        <Greeting saludo='Maria' />
-        <Button
-          onPress={() => {
-          Alert.alert('Alerta desbloqueada!');
-          }}
-          title="Presioname"
+      <View style={styles.container}>
+        <Text>Colores</Text>
+        <FlatList
+          data = {[
+            {key: 'Rojo'},
+            {key: 'Negro'},
+            {key: 'Blanco'},
+            {key: 'Verde'},
+            {key: 'Amarillo'},
+            {key: 'Azul'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
         />
-        <TextInput
-          style={{height: 40}}
-          placeholder="Traductor pizzero"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Image source={pic} style={{width: 350, height: 200}}/>
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-        </Text>
-        <Text style = {styles.yellow}>Mira las bananas arriba</Text>
-        <Blink text = 'Yo parpadeo'/>
-      </ScrollView>   
+      </View>
+        
     );
   }
   
 }
 
 const styles = StyleSheet.create({
-  smallgreen: {
-    color: 'green',
-    fontSize: 10,
-  },
-  yellow: {
-    color: 'yellow',
-    fontSize: 100,
-  },
+  container: {
+    flex: 1,
+    paddingTop: 22
+   },
+   item: {
+     padding: 10,
+     fontSize: 18,
+     height: 44,
+   },
 });
