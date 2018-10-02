ae6328d2dd1675abf3fc9c4b0189bf059343b931
Julio Alfonso Rodriguez Olea <julio.rodriguez@cetys.edu.mx>
2018-10-02 12:46:26 -0700

    Props

--- Basics/App.js
+++ Basics/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default class App extends React.Component {
+class Greeting extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-      </View>
+      <Text>Buenos dias {this.props.saludo}!</Text>
     );
   }
+}
+
+export default class App extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting saludo='Roxanne' />
+        <Greeting saludo='Henry' />
+        <Greeting saludo='Maria' />
+        <Image source={pic} style={{width: 193, height: 140}}/>
+      </View>   
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
